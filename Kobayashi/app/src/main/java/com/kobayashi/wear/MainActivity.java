a503e79edb191cf02690871aba50ae9922f21726
hidecheck <hidecheck@gmail.com>
2014-06-14 13:52:35 +0900

    edit gradle

--- Kobayashi/app/src/main/java/com/kobayashi/wear/MainActivity.java
+++ Kobayashi/app/src/main/java/com/kobayashi/wear/MainActivity.java
@@ -1,9 +1,14 @@
 package com.kobayashi.wear;
 
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.preview.support.v4.app.NotificationManagerCompat;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -32,5 +37,34 @@ public class MainActivity extends ActionBarActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void onClcikNotification(View v){
+        int notificationId = 001;
+        int eventId = 1;
+        String eventTitle = "title";
+        String eventMessate = "message";
+
+        Intent viewIntent = new Intent(this, MainActivity.class);
+        viewIntent.putExtra("eventId", eventId);
+        PendingIntent viewPendingIntent =
+                PendingIntent.getActivity(this, 0, viewIntent, 0);
+
+        NotificationCompat.Builder notificationBuilder =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.drawable.ic_launcher)
+                        .setContentTitle(eventTitle)
+                        .setContentText(eventMessate)
+                        .setContentIntent(viewPendingIntent);
+
+        NotificationManagerCompat notificationManager =
+                NotificationManagerCompat.from(this);
+
+        notificationManager.notify(notificationId, notificationBuilder.build());
+
+    }
+
+    private void sendNotification(){
+
     }
 }
