788687cafda661a39a5b537d384a46215a71af2d
hidecheck <hidecheck@gmail.com>
2014-06-14 13:55:50 +0900

    add Constans class

---
+++ Kobayashi/app/src/main/java/com/kobayashi/wear/Constants.java
@@ -0,0 +1,12 @@
+package com.kobayashi.wear;
+
+/**
+ * Created by hidecheck on 2014/06/14.
+ */
+public class Constants {
+    public static byte DOTS = 0;
+    public static byte DASHS = 1;
+    public static byte SPACE = 2;
+    public static byte END = 3;
+
+}
